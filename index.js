3e56adbb19769b375b4e11e126c68bea2ca12d98
paulmunyao <paulmunyao094@gmail.com>
2022-08-07 18:17:08 +0300

    class for input text

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const money_plus = document.getElementById('money-plus');
 const money_minus = document.getElementById('money-minus');
 const list = document.getElementById('list');
 const form = document.getElementById('form');
+const text = document.getElementById('text');
