8e11fbbdf00a87b3c269f8a836ac3da4c7fbf602
paulmunyao <paulmunyao094@gmail.com>
2022-08-07 18:30:41 +0300

    transactions to be stored in a new empty array

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ const localStorageTransactions = JSON.parse(
   localStorage.getItem("transactions")
 );
 
-
+let transactions = localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
