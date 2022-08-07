2faa0908f30188f4f6ead0896e306b1ef0329efd
paulmunyao <paulmunyao094@gmail.com>
2022-08-07 19:23:52 +0300

    edit code

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ function addTransactionDOM(transaction) {
   item.classList.add(transaction.amount < 0 ? "minus" : "plus");
 
   item.innerHTML = `
-//   ${transaction.text} <span>${sign}${Math.abs(
-//     transaction.amount
+  ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
   )}</span> <button class="delete-btn" onclick="removeTransaction(${
     transaction.id
   })">x</button>
