44881a69d60cc2c7ff4b0e4224089fc53fb37997
paulmunyao <paulmunyao094@gmail.com>
2022-08-07 18:25:15 +0300

    get transactions history from local storage

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
