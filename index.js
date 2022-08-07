ca362245d853dae9d49c195820271e9ea8008394
paulmunyao <paulmunyao094@gmail.com>
2022-08-08 00:48:28 +0300

    edit code

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function updateValues() {
 
   const income = amounts
     .filter((item) => item > 0)
-    .reduce((acc, item) => (acc += item), 0)
+    .reduce((acc, item) => (acc = item), 0)
     .toFixed(2);
 
   const expense = (
