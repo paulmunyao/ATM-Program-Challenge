8a9b3692bbaa721e27fe75c5e9f3e5873b03bc62
paulmunyao <paulmunyao094@gmail.com>
2022-08-07 19:15:58 +0300

    update code

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let transactions =
 function addTransaction(transaction) {
   transaction.preventDefault();
 
-  if (text.value.trim() === "" || amount.value.trim() === "") {
+  if (amount.value.trim() === "") {
     alert("Please add amount you wish to transact");
   } else {
     const transaction = {
@@ -49,3 +49,50 @@ function addTransactionDOM(transaction) {
 
   list.appendChild(item);
 }
+
+// Update the balance, income and expense
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+  
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  
+    const income = amounts
+      .filter(item => item > 0)
+      .reduce((acc, item) => (acc += item), 0)
+      .toFixed(2);
+  
+    const expense = (
+      amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+      -1
+    ).toFixed(2);
+  
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+  }
+  
+  // Remove transaction by ID
+  function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id);
+  
+    updateLocalStorage();
+  
+    init();
+  }
+  
+  // Update local storage transactions
+  function updateLocalStorage() {
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+  }
+  
+  // Init app
+  function init() {
+    list.innerHTML = '';
+  
+    transactions.forEach(addTransactionDOM);
+    updateValues();
+  }
+  
+  init();
+  
+  form.addEventListener('submit', addTransaction);
