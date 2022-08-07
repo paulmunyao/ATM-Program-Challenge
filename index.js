a988def750c803eaccba6ef280009c8045ab16a2
paulmunyao <paulmunyao094@gmail.com>
2022-08-07 18:16:06 +0300

    class for input forms

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const balance = document.getElementById('balance');
 const money_plus = document.getElementById('money-plus');
 const money_minus = document.getElementById('money-minus');
 const list = document.getElementById('list');
+const form = document.getElementById('form');
