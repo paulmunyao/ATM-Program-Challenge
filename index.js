57ce6ebf75d06983b889a85294e0887a406cb64a
paulmunyao <paulmunyao094@gmail.com>
2022-08-10 22:43:33 +0300

    comment code

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const localStorageTransactions = JSON.parse(
 let transactions =
   localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
 
-  
+// function for cash deposit
 function addTransaction(transaction) {
   transaction.preventDefault();
 
