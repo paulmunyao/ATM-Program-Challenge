e63ca1fad6a1109614a625d618edafe79db7bfc8
paulmunyao <paulmunyao094@gmail.com>
2022-08-11 23:50:43 +0300

    edit transactions parameters functions

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let transactions =
   localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
 
 // function for cash deposit
-function addTransaction(_transaction) {
+function addTransaction(transaction) {
   // transaction.preventDefault();
 
   if (amount.value === "") {
