b11817eeed5cd36eece4e086b00cc602f8ec2913
paulmunyao <paulmunyao094@gmail.com>
2022-08-12 16:31:44 +0300

    edit function name

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ form.addEventListener("submit", addTransaction);
 function init() {
   list.innerHTML = "";
 
-  transactions.forEach(TransactionDOM);
+  transactions.forEach(withdrawalTransactionDOM);
   updateValues();
 }
 
