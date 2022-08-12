025e94c5578f8ad1b5615f243958d530e20b91bd
paulmunyao <paulmunyao094@gmail.com>
2022-08-12 16:52:01 +0300

    edit const for withdrawal of transaction

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ function updateValues() {
     .reduce((acc, item) => (acc = item), 0)
     .toFixed(2);
 
-  const withdrawal = (
+  const  = (
     amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
     -1
   ).toFixed(2);
