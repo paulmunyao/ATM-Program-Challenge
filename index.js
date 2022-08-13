6c784fa7dbc86873605a5dfb888e0c32e3a5d1d7
paulmunyao <paulmunyao094@gmail.com>
2022-08-13 23:57:54 +0300

    new code line

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 
+
 const localStorageTransactions = JSON.parse(
   localStorage.getItem("transactions")
 );
