0993750e99350904e724b3e519f777e27442b917
paulmunyao <paulmunyao094@gmail.com>
2022-08-14 22:34:46 +0300

    edit const name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const list = document.getElementById("list");
 const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
-const withdraw = document.getElementById("ithdraw");
+const withdraw = document.getElementById("withdraw");
 
 const localStorageTransactions = JSON.parse(
   localStorage.getItem("transactions")
