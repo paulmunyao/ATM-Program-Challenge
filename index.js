cac9932a10770bcd1e2567ea876641700d3864f3
paulmunyao <paulmunyao094@gmail.com>
2022-08-14 22:53:52 +0300

    edit transaction value

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function withdrawTransactionDOM(transaction) {
   const sign = transaction.withdraw < 0 ? "-" : "+";
   const item = document.createElement("li");
 
-  item.classList.remove(transaction. < 0 ? "minus" : "plus");
+  item.classList.remove(transaction.withdraw < 0 ? "minus" : "plus");
 
   item.innerHTML = `  
   <span>${sign}${Math.abs(
